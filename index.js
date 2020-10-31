75a1ef51dcc616160f071d5ab8bda51821e2f27a
Luis Pablo <wichos11.ch@gmail.com>
2020-10-30 23:27:30 -0600

    balanceador

--- index.js
+++ index.js
@@ -1,33 +1,247 @@
 const express = require('express')
 var body_parser = require('body-parser');
-
+const https = require('https');
 
 const app = express()
+const fetch = require('node-fetch');
+const { dir } = require('console');
 const port = 3000
 app.use(body_parser.json({ limit: '5mb', extended: true }));
 
+var direccionamiento = 1
+
+var maquinalocal= "1493ba5945eb.ngrok.io";
+var maquinanube= "3.80.201.217:3000";
 
 app.get('/', (req, res) => {
   res.send('Balanceador!')
 })
-app.post('/login', (req, res) => {
-    console.log(req.body)
-    var user = req.body.user ;
-    var password = req.body.password ;
-    res.send('Login '+user+"   "+password+ " $")
+app.post('/login', async (req, res) => {
+
+    if (direccionamiento==1){
+        try {
+            const response = await fetch("http://"+maquinalocal+"/login", {
+                method: 'post',
+                body: JSON.stringify(req.body),
+                headers: {'Content-Type': 'application/json'}
+            });
+            const body = await response.json();
+            direccionamiento=0
+            res.send(body)
+        } catch (error) {
+            
+
+            try {
+                const response = await fetch("http://"+maquinanube+"/login", {
+                    method: 'post',
+                    body: JSON.stringify(req.body),
+                    headers: {'Content-Type': 'application/json'}
+                });
+                const body = await response.json();
+                direccionamiento=1
+                res.send(body)
+
+                
+            } catch (error) {
+                res.send("No hay ningun servidor disponible")
+            }
+        }
+
+        
+
+        
+    }else{
+        try {
+            const response = await fetch("http://"+maquinanube+"/login", {
+                method: 'post',
+                body: JSON.stringify(req.body),
+                headers: {'Content-Type': 'application/json'}
+            });
+            const body = await response.json();
+            direccionamiento=0
+            res.send(body)
+        } catch (error) {
+            
+            try {
+                const response = await fetch("http://"+maquinalocal+"/login", {
+                    method: 'post',
+                    body: JSON.stringify(req.body),
+                    headers: {'Content-Type': 'application/json'}
+                });
+                const body = await response.json();
+                direccionamiento=1
+                res.send(body)
+
+                
+            } catch (error) {
+                res.send("No hay ningun servidor disponible")
+            }
+        }
+
+    }
   })
 
-  app.post('/registro', (req, res) => {
-    var datos = req.body.datos || '';
-    res.send('Registro ' +datos)
+
+
+
+  app.post('/registro', async (req, res) => {
+    if (direccionamiento==1){
+        try {
+            const response = await fetch("http://"+maquinalocal+"/registro", {
+                method: 'post',
+                body: JSON.stringify(req.body),
+                headers: {'Content-Type': 'application/json'}
+            });
+            const body = await response.json();
+            direccionamiento=0
+            res.send(body)
+        } catch (error) {
+
+            try {
+                const response = await fetch("http://"+maquinanube+"/registro", {
+                    method: 'post',
+                    body: JSON.stringify(req.body),
+                    headers: {'Content-Type': 'application/json'}
+                });
+                const body = await response.json();
+                direccionamiento=1
+                res.send(body)
+
+                
+            } catch (error) {
+                res.send("No hay ningun servidor disponible")
+            }
+        }
+
+        
+
+        
+    }else{
+        try {
+            const response = await fetch("http://"+maquinanube+"/registro", {
+                method: 'post',
+                body: JSON.stringify(req.body),
+                headers: {'Content-Type': 'application/json'}
+            });
+            const body = await response.json();
+            direccionamiento=0
+            res.send(body)
+        } catch (error) {
+        
+            try {
+                const response = await fetch("http://"+maquinalocal+"/registro", {
+                    method: 'post',
+                    body: JSON.stringify(req.body),
+                    headers: {'Content-Type': 'application/json'}
+                });
+                const body = await response.json();
+                direccionamiento=1
+                res.send(body)
+
+                
+            } catch (error) {
+                res.send("No hay ningun servidor disponible")
+            }
+        }
+
+    }
   })
 
-  app.get('/server', (req, res) => {
-    res.send('Servidor')
+  app.get('/server', async (req, res) => {
+    if (direccionamiento==1){
+        try {
+            const response = await fetch("http://"+maquinalocal+"/server");
+            const body = await response.json();
+            direccionamiento=0
+            res.send(body)
+        } catch (error) {
+
+            try {
+                const response = await fetch("http://"+maquinanube+"/server");
+                const body = await response.json();
+                direccionamiento=1
+                res.send(body)
+
+                
+            } catch (error) {
+                res.send("No hay ningun servidor disponible")
+            }
+        }
+
+        
+
+        
+    }else{
+        try {
+            const response = await fetch("http://"+maquinanube+"/server");
+            const body = await response.json();
+            direccionamiento=0
+            res.send(body)
+        } catch (error) {
+        
+            try {
+                const response = await fetch("http://"+maquinalocal+"/server");
+                const body = await response.json();
+                direccionamiento=1
+                res.send(body)
+
+                
+            } catch (error) {
+                res.send("No hay ningun servidor disponible")
+            }
+        }
+
+    }
   })
 
-  app.get('/funcion', (req, res) => {
-    res.send('Funcion')
+
+  app.get('/funcion', async (req, res) => {
+
+    if (direccionamiento==1){
+        try {
+            const response = await fetch("http://"+maquinalocal+"/funcion");
+            const body = await response.json();
+            direccionamiento=0
+            res.send(body)
+        } catch (error) {
+
+            try {
+                const response = await fetch("http://"+maquinanube+"/funcion");
+                const body = await response.json();
+                direccionamiento=1
+                res.send(body)
+
+                
+            } catch (error) {
+                res.send("No hay ningun servidor disponible")
+            }
+        }
+
+        
+
+        
+    }else{
+        try {
+            const response = await fetch("http://"+maquinanube+"/funcion");
+            const body = await response.json();
+            direccionamiento=0
+            res.send(body)
+        } catch (error) {
+        
+            try {
+                const response = await fetch("http://"+maquinalocal+"/funcion");
+                const body = await response.json();
+                direccionamiento=1
+                res.send(body)
+
+                
+            } catch (error) {
+                res.send("No hay ningun servidor disponible")
+            }
+        }
+
+    }
+     
   })
 
   
